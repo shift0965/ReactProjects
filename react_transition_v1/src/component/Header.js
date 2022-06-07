341c7b8a852551924a1b2739068299a3d69142ff
shift0965 <shift0965@gmail.com>
2022-06-07 23:00:57 +0800

    input all the files

---
+++ react_transition_v1/src/component/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styled from "styled-components";
+
+const Navbar = styled.nav`
+    height: 60px;
+    background: red;
+`
+
+const Header = () =>{
+    return (
+        <Navbar>
+        </Navbar> 
+    )
+}
+
+export default Header;
