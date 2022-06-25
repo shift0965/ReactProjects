4c974390db61012c6848966811c4656eae133b2a
Jackie Chen <jackie@Jackies-Air.Home>
2022-06-25 22:14:24 +0800

    SignIn page set up and little style

--- sorting_algorithm/src/components/ArrayBar.js
+++ sorting_algorithm/src/components/ArrayBar.js
@@ -1,7 +1,10 @@
 import { StyledArrayBar } from "./styles/ArrayBar.styled";
+import { useState, useEffect} from "react";
 
 
 export default function ArrayBar({number}) {
+    const [len, setLen] = useState;
+    
 
     return (   
         <StyledArrayBar number = {number} style={{height: `${number}px`}}>
